13e6e86d1e1b08fa6562d9aff1e4dfe8f1193bf6
Emil Abraham <emilabraham1414@gmail.com>
2019-07-29 14:30:31 -0400

    Converting 3 digits

--- p17/hundredsToWords.py
+++ p17/hundredsToWords.py
@@ -3,7 +3,7 @@
 ones = { 0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7:
         "sevin", 8: "eight", 9: "nine" }
 
-tens = { 2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7:
+tens = { 0: "", 1: "ten", 2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7:
         "seventy", 8: "eighty", 9: "ninety" }
 
 specialTens = { 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15:
@@ -11,17 +11,26 @@ specialTens = { 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15:
         "nineteen" }
 
 def convertToWord(number):
-    stringRepresentation = str(number)
-    print ones[number]
+    return hundredsWord(number) + tensWord(number)
 
 #number : string
 # returns English representation of the hundreds place
 def hundredsWord(number):
     if (int(number) > 100):
-        return ones[int(number[0])] + " hundred"
+        return ones[int(number[0])] + "hundred"
     else:
         return ""
 
-print hundredsWord("101")
-print hundredsWord("099")
-print hundredsWord("999")
+#number : stirng
+# returns English representation of the tens and ones place
+def tensWord(number):
+    if(int(number[1:]) > 10 and int(number[1:]) < 20):
+        return specialTens[int(number[1:])]
+    else:
+        return tens[int(number[1])] + ones[int(number[2])]
+
+print convertToWord("101")
+print convertToWord("112")
+print convertToWord("110")
+print convertToWord("120")
+print convertToWord("128")
